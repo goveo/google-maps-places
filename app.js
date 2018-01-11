344e1ed2588b5413f8a07050ef548babd7cbd7df
goveo <yuriybrusentsov@gmail.com>
2018-01-11 22:15:25 +0200

    deleted unused methods

--- app.js
+++ app.js
@@ -75,56 +75,6 @@ var app = new Vue({
             });
             app.map.fitBounds(bounds);
         },
-        processResults: function (results, status, pagination) {
-            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                return;
-            } else {
-                this.deleteAllMarkers();
-                this.createMarkers(results);
-            }
-        },
-        createMarkers: function (places) {
-            app.placesList = []; // null
-            console.log('placesList : ', this.placesList);
-
-            var bounds = new google.maps.LatLngBounds();
-
-            var placesListEl = document.getElementById('places');
-
-            for (var i = 0, place; place = places[i]; i++) {
-                var image = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
-
-                var marker = new google.maps.Marker({
-                    map: this.map,
-                    icon: image,
-                    title: place.name,
-                    position: place.geometry.location
-                });
-                let currentPlace = {
-                    position: place.geometry.location,
-                    name: place.name
-                }
-
-                marker.addListener('click', function () {
-                    app.map.setCenter(marker.getPosition());
-                    alert(`do you wanna add ${currentPlace.name} ?`);
-                    // process info about place
-                    myPlaces.push(currentPlace);
-                });
-
-                this.markers.push(marker);
-                app.placesList.push(currentPlace);
-
-                bounds.extend(place.geometry.location);
-            }
-            this.map.fitBounds(bounds);
-        },
         setMapOnAll: function (map) {
             for (var i = 0; i < this.markers.length; i++) {
                 this.markers[i].setMap(map);
